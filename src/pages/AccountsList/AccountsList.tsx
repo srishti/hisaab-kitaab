ca99e9333b1b43e5cebdf0b3745ac3cd3596c4e7
Srishti Gupta <mail@srishtigupta.in>
2021-08-13 14:01:21 +0530

    integrated AccountsList page with Firebase CloudStore

--- src/pages/AccountsList/AccountsList.tsx
+++ src/pages/AccountsList/AccountsList.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { accounts } from "./accountsData";
 import { RoutePath } from "../../routes/routes";
+import { useHttp } from "../../hooks/http/use-http";
+import { Account } from "../../models/account";
+import * as httpConfig from "../../hooks/http/http";
 import Button from "../../components/UI/Button/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
@@ -9,6 +11,25 @@ import AccountListItem from "./AccountListItem";
 import styles from "./AccountsList.module.scss";
 
 const Accounts: React.FC = () => {
+  const [accounts, setAccounts] = useState<Account[]>([]);
+  const http = useHttp();
+
+  useEffect(() => {
+    const requestConfig: httpConfig.RequestConfig = {
+      url: httpConfig.BASE_URL + httpConfig.PathParameters.Accounts,
+      config: {
+        method: httpConfig.HttpMethod.GET,
+      },
+    };
+
+    const fetchAccountsSuccessCallback = (data: any[]) => {
+      console.log("Fetched all accounts successfully!", data);
+      setAccounts(data);
+    };
+
+    http.sendRequest(requestConfig, fetchAccountsSuccessCallback);
+  }, []);
+
   return (
     <section className={styles["accounts-list"]}>
       <Link to={RoutePath.AddAccount}>
